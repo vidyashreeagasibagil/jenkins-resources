7ae3509440e6df1add55d36f4661367aa9921ac3
vidyashreeagasibagil <64072083+vidyashreeagasibagil@users.noreply.github.com>
2022-01-27 21:57:33 +0530

    Update deploy.sh

--- jenkins/pipeline/jenkins/deploy/deploy.sh
+++ jenkins/pipeline/jenkins/deploy/deploy.sh
@@ -6,4 +6,4 @@ echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
 scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+ssh -o StrictHostKeyChecking=no prod-user@192.168.43.247 "/tmp/publish"
